057d897023f2ed542c54e2f77677bfe81dd13d44
Samwoo Seong <tjdtkadn@gmail.com>
2020-07-01 21:39:36 -0700

    Joint Control Practice
    Joint control practice with default controllers provided by ROS Controller package and a customized controller.

---
+++ catkin_ws/build/project_controller/CMakeFiles/project_controller_lib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_controller_lib.dir/src/project_controller1.cpp.o"
+  "/home/user/catkin_ws/devel/lib/libproject_controller_lib.pdb"
+  "/home/user/catkin_ws/devel/lib/libproject_controller_lib.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_controller_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
